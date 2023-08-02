7cd175ef5a3192bcb45b29192b989e689fd36e12
georgenaguiat <rnaguiat0627@me.com>
2023-08-02 13:49:31 -0700

    Complete assignment Hello World

---
+++ fundamentals/fundamentals/hello_world.py
@@ -0,0 +1,15 @@
+# 1. TASK: print "Hello World"
+print('Hello World' )
+
+name = 'George'
+print('Hello', name)
+print('Hello ' + name)
+
+favNumber = 10
+print('Hello', favNumber)
+print('Hello ' + str(favNumber))
+
+fave_food1 = 'hamburger'
+fave_food2 = 'pizza'
+print('I love to eat {} and {}.'.format(fave_food1, fave_food2))
+print(f'I love to eat {fave_food1} and {fave_food2}.')
