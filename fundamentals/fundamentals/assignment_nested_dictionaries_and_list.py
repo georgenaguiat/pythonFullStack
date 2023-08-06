1505936a654b50ff7bd27f993906ab7038cced78
georgenaguiat <rnaguiat0627@me.com>
2023-08-06 16:26:12 -0700

    Assignment Nested Dictionaries And List

---
+++ fundamentals/fundamentals/assignment_nested_dictionaries_and_list.py
@@ -0,0 +1,77 @@
+# Update Values in Dictionaries and Lists
+x = [ [5,2,3], [10,8,9] ] 
+students = [
+    {'first_name':  'Michael', 'last_name' : 'Jordan'},
+    {'first_name' : 'John', 'last_name' : 'Rosales'}
+]
+sports_directory = {
+    'basketball' : ['Kobe', 'Jordan', 'James', 'Curry'],
+    'soccer' : ['Messi', 'Ronaldo', 'Rooney']
+}
+z = [ {'x': 10, 'y': 20} ]
+
+# Change the value 10 in x to 15. Once you're done, x should now be [ [5,2,3], [15,8,9] ]
+x[1][0] = 15
+print(x)
+
+# Change the last_name of the first student from 'Jordan' to 'Bryant'
+students[0]['last_name'] = 'Bryant'
+print(students[0])
+
+# In the sports_directory, change 'Messi' to 'Andres'
+sports_directory['soccer'][0] = 'Andres'
+print(sports_directory)
+
+# Change the value 20 in z to 30
+z[0]['y'] = 30
+print(z)
+print('----------------------------')
+
+# Iterate Through a List of Dictionaries
+students = [
+        {'first_name':  'Michael', 'last_name' : 'Jordan'},
+        {'first_name' : 'John', 'last_name' : 'Rosales'},
+        {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+        {'first_name' : 'KB', 'last_name' : 'Tonel'}
+    ]
+
+def interateDictionary(some_list):
+    for student in some_list:
+        student_list = '#'.join(f'{key} - {value}' for key, value in student.items())
+        print(student_list)
+    
+
+interateDictionary(students)
+print('----------------------------')
+
+# Get Values From a List of Dictionaries
+studentsName = [
+        {'first_name':  'Michael', 'last_name' : 'Jordan'},
+        {'first_name' : 'John', 'last_name' : 'Rosales'},
+        {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+        {'first_name' : 'KB', 'last_name' : 'Tonel'}
+    ]
+
+def interateDictionary2(key_name,some_list):
+    for name in some_list:
+        print(name.get(key_name))
+
+interateDictionary2('first_name', studentsName)
+print('----------------------------')
+interateDictionary2('last_name', studentsName)
+print('----------------------------')
+
+# Iterate Through a Dictionary with List Values
+dojo = {
+    'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
+    'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
+}
+
+def printinfo(dojo_dict):
+    for key_name in dojo_dict:
+        print(f'{len(dojo_dict[key_name])} {key_name.upper()}')
+        for value in dojo_dict[key_name]:
+            print(value)
+        print()
+
+printinfo(dojo)
