62b0bf7dc14219dca771e1ef75732a5c9c823918
georgenaguiat <rnaguiat0627@me.com>
2023-08-10 22:12:47 -0700

    Update Assignment User

--- fundamentals/fundamentals/assignment_user.py
+++ fundamentals/fundamentals/assignment_user.py
@@ -1,4 +1,5 @@
 class User:
+
     def __init__(self, first_name, last_name,email, age, is_reward_member=False, gold_card_points=0):
         self.first_name = first_name
         self.last_name = last_name
@@ -8,12 +9,14 @@ class User:
         self.gold_card_points = gold_card_points
 
     def display_info(self):
+        print('============================================')
         print('First Name:', self.first_name)
         print('Last Name:', self.last_name)
         print('Email:', self.email)
         print('Age:', self.age)
         print('Reward Member:', self.is_reward_member)
         print('Gold Card Points:', self.gold_card_points)
+        print('============================================')
 
     def enrollment(self):    
         if self.is_reward_member:
@@ -21,7 +24,7 @@ class User:
             return False
         else:
             self.is_reward_member = True
-            self.is_reward_member = 200
+            self.gold_card_points = 200
             return True
 
     def spend_points(self, amount):
@@ -31,13 +34,11 @@ class User:
             self.gold_card_points -= amount
 
 person = User('Ryan', 'Kai', 'thisismyemail@yahoo.com', 21)
-person1 = User('Chris', 'Johnson', 'myemail@gmail.com', 200)
+person1 = User('Chris', 'Johnson', 'myemail@gmail.com', 30)
 person.enrollment()
 person.enrollment()
 person1.enrollment()
 person.spend_points(50)
-person1.spend_points(80)
+person1.spend_points(100)
 person.display_info()
 person1.display_info()
-    
-    
