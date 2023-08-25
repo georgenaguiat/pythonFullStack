ce05c5052182e9c86cc5a4cd4e77c1aae5bdc06d
georgenaguiat <rnaguiat0627@me.com>
2023-08-24 18:54:46 -0700

    Completed Practice Assignment Checknboard

---
+++ flask/fundamentals/checkerboard/server.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template # Import Flask to allow us to create our app
+app = Flask(__name__)
+
+@app.route('/')
+def checker():
+    return render_template('checkerboard.html')
+
+@app.route('/<int:x>')
+def checker_box(x):
+    return render_template('checkerboard.html', rows=x)
+
+if __name__=='__main__': # Ensure this file is being run directly and not from a different module
+    app.run(debug=True, host="localhost", port=8000)
