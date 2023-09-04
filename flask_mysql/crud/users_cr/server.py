28dff873bbc84dc604e57a6d3b32cb24263a1d0d
georgenaguiat <rnaguiat0627@me.com>
2023-09-04 01:45:20 -0700

    Completed Assignment User CRUD

--- flask_mysql/crud/users_cr/server.py
+++ flask_mysql/crud/users_cr/server.py
@@ -20,7 +20,39 @@ def create_user():
         }
     User.save(request.form)
     return redirect('/')
-    
+
+@app.route('/user/show/<int:user_id>')
+def show_user(user_id):
+    data = {
+        'id': user_id
+    }
+    return render_template('show.html', get_user=User.get_one(data))
+
+@app.route('/user/edit/<int:user_id>')
+def edit(user_id):
+    data = {
+        'id': user_id
+    }
+    return render_template('edit.html', edit_user = User.get_one(data))
+
+@app.route('/user/update/<int:user_id>', methods=['POST'])
+def update(user_id):
+    data = {
+        'id': user_id,
+        'first_name': request.form['first_name'],
+        'last_name': request.form['last_name'],
+        'email': request.form['email']
+    }
+    User.update(data)
+    return redirect(f'/user/show/{user_id}')
+
+@app.route('/user/delete/<int:user_id>')
+def delete(user_id):
+    data = {
+        'id': user_id
+    }
+    User.destroy(data)
+    return redirect('/')
 
 if __name__== "__main__":
     app.run(debug=True, host='localhost', port=8000)
